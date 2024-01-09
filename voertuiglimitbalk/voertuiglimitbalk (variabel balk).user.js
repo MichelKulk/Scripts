566afab35e31704142bd89da35d26e35a5fce192
MichelKulk <126677435+MichelKulk@users.noreply.github.com>
2024-01-09 23:52:40 +0100

    Update voertuiglimitbalk (variabel balk).user.js

--- voertuiglimitbalk/voertuiglimitbalk (variabel balk).user.js
+++ voertuiglimitbalk/voertuiglimitbalk (variabel balk).user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       Michel
 // @match        https://www.meldkamerspel.com/missions/*
+// @updateURL    https://raw.githubusercontent.com/MichelKulk/Scripts/main/voertuiglimitbalk/voertuiglimitbalk%20(variabel%20balk).user.js
 // @grant        none
 // ==/UserScript==
 
