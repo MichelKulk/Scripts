01a4b87060d98e74aaad36b845cbed3507a4d05e
MichelKulk <126677435+MichelKulk@users.noreply.github.com>
2024-01-10 08:16:50 +0100

    Update voertuiglimitbalk (variabel balk).user.js
    Tekstuele aanpassingen in het script

--- voertuiglimitbalk/voertuiglimitbalk (variabel balk).user.js
+++ voertuiglimitbalk/voertuiglimitbalk (variabel balk).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         voertuiglimitbalk (variabel balk)
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  try to take over the world!
 // @author       Michel
 // @match        https://www.meldkamerspel.com/missions/*
@@ -9,12 +9,16 @@
 // @downloadURL  https://raw.githubusercontent.com/MichelKulk/Scripts/main/voertuiglimitbalk/voertuiglimitbalk%20(variabel%20balk).user.js
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
-    const numberOfTimes = 3; // Het aantal keer dat de functie moet worden aangeroepen
-    let clickCount = 0; // Houdt bij hoe vaak er geklikt is
 
-// hieronder niks meer aanpassen
+// vul hier de aantal keer in dat de balk automatisch ingedrukt moet worden.
+
+    const numberOfTimes = 3; 
+
+// hieronder niks meer aanpassen!
+
+    let clickCount = 0; 
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             if (mutation.type === 'attributes' && (mutation.attributeName === 'class' || mutation.attributeName === 'style')) {
